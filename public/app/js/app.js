2158755e0c0771b14335297e6926d4a1364176b9
hanfi <souhail.hanfi@neoxia.com>
2013-06-06 18:40:27 +0200

    it's redirectTo not redirect :D

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,7 +2,7 @@ angular.module("myApp",['ngResource','ngSanitize'])
     .config(['$routeProvider',function($routeProvider){
         $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'})
         $routeProvider.when('/home',{templateUrl:'app/partials/home.html', controller: 'homeController'})
-        $routeProvider.otherwise({redirect:'/'})
+        $routeProvider.otherwise({redirectTo :'/'})
     }]).config(function($httpProvider){
 
         var interceptor = function($rootScope,$location,$q,Flash){
